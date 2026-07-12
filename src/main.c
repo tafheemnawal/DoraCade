aeda1ae7fe6a6f581ba993b5df0d753989a902b9
tafheemnawal <tafheemnawal.crs@gmail.com>
2026-07-12 07:03:22 +0600

    v0.2.0 - Refactor player update and frame-independent movement

--- src/main.c
+++ src/main.c
@@ -1,4 +1,53 @@
 #include <raylib.h>
+#define PLAYER_SIZE 50
+typedef struct
+{
+    float x;
+    float y;
+    float speed;
+} Player;
+void UpdatePlayer(Player *player, int screenWidth, int screenHeight, float dt)
+{ // update
+    if (IsKeyDown(KEY_D))
+    {
+        player->x += player->speed * dt;
+    }
+    if (IsKeyDown(KEY_A))
+    {
+        player->x -= player->speed * dt;
+    }
+
+    if (IsKeyDown(KEY_W))
+    {
+        player->y -= player->speed * dt;
+    }
+
+    if (IsKeyDown(KEY_S))
+    {
+        player->y += player->speed * dt;
+    }
+
+    // Keep player inside the window
+    if (player->x < 0)
+    {
+        player->x = 0;
+    }
+
+    if (player->x > screenWidth - PLAYER_SIZE)
+    {
+        player->x = screenWidth - PLAYER_SIZE;
+    }
+
+    if (player->y < 0)
+    {
+        player->y = 0;
+    }
+
+    if (player->y > screenHeight - PLAYER_SIZE)
+    {
+        player->y = screenHeight - PLAYER_SIZE;
+    }
+}
 
 int main(void)
 {
@@ -7,14 +56,29 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "DoraCade");
 
+    Player player;
+
+    player.x = 400;
+    player.y = 300;
+    player.speed = 300;
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        float dt = GetFrameTime();
+        UpdatePlayer(&player, screenWidth, screenHeight, dt);
+
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-
+        DrawRectangle(
+            player.x,
+            player.y,
+            PLAYER_SIZE,
+            PLAYER_SIZE,
+            BLUE);
+        DrawFPS(10, 10);
         DrawText("Welcome to DoraCade!", 420, 300, 40, BLUE);
         DrawText("Level 1", 560, 360, 30, RED);
 
