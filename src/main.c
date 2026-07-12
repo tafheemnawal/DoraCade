316c553d150c883ea8cea158310077d676b32b68
tafheemnawal <tafheemnawal.crs@gmail.com>
2026-07-12 07:50:34 +0600

    v0.2.0 - Refactor player into init, update and draw system

--- src/main.c
+++ src/main.c
@@ -60,44 +60,47 @@ void DrawPlayer(Player *player)
         player->y,
         player->width,
         player->height,
-        BLUE);
+        player->color);
 }
-int main(void)
+void InitPlayer(Player *player, int screenWidth, int screenHeight)
 {
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
-
-    InitWindow(screenWidth, screenHeight, "DoraCade");
-
-    Player player;
-
-    player.x = 400;
-    player.y = 300;
-    player.speed = 300;
-    player.width = 50;
-    player.height = 50;
-    player.color = BLUE;
-
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose())
-    {
-        float dt = GetFrameTime();
-        UpdatePlayer(&player, screenWidth, screenHeight, dt);
-
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawPlayer(&player);
-        DrawFPS(10, 10);
-        DrawText("Welcome to DoraCade!", 420, 300, 40, BLUE);
-        DrawText("Level 1", 560, 360, 30, RED);
-
-        EndDrawing();
-    }
-
-    CloseWindow();
-
-    return 0;
+    player->width = 50;
+    player->height = 50;
+    player->x = (screenWidth - player->width) / 2;
+    player->y = (screenHeight - player->height) / 2;
+    player->speed = 300;
+    player->color = BLUE;
 }
+    int main(void)
+    {
+        const int screenWidth = 1280;
+        const int screenHeight = 720;
+
+        InitWindow(screenWidth, screenHeight, "DoraCade");
+
+        Player player;
+
+        InitPlayer(&player, screenWidth, screenHeight);
+        SetTargetFPS(60);
+
+        while (!WindowShouldClose())
+        {
+            float dt = GetFrameTime();
+            UpdatePlayer(&player, screenWidth, screenHeight, dt);
+
+            BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawPlayer(&player);
+            DrawFPS(10, 10);
+            DrawText("Welcome to DoraCade!", 420, 300, 40, BLUE);
+            DrawText("Level 1", 560, 360, 30, RED);
+
+            EndDrawing();
+        }
+
+        CloseWindow();
+
+        return 0;
+    }
