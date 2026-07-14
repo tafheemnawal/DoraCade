08dcdca5f9da09fe999cd8670df41ccd4a7d4133
OnannaIslamFiha <fihaonannaislam01@gmail.com>
2026-07-14 18:44:44 +0600

    logic correction

--- src/GadgetInvaders/gadget_invaders.c
+++ src/GadgetInvaders/gadget_invaders.c
@@ -1,5 +1,4 @@
 #include "gadget_invaders.h"
-#include <stdlib.h>
 
 static int GetGadgetPoints(GadgetType type)
 {
@@ -56,40 +55,18 @@ Gadget CreateGadget()
 {
     Gadget gadget;
 
-    // Random gadget type (5 types)
+    // Random gadget type
     gadget.type = GetRandomValue(0, 4);
 
     // Starting position
-    gadget.position.x = GetRandomValue(50, 750); 
+    gadget.position.x = GetRandomValue(50, 750);
     gadget.position.y = -50;
 
-    // High falling speed
+    // Falling speed
     gadget.speed = GetRandomValue(5, 10);
 
-    // Assign points
-    switch(gadget.type)
-    {
-        case ANYWHERE_DOOR:
-            gadget.points = 50;
-            break;
-
-        case BAMBOO_COPTER:
-            gadget.points = 10;
-            break;
-
-        case TIME_MACHINE:
-            gadget.points = 100;
-            break;
-
-        case SMALL_LIGHT:
-            gadget.points = 20;
-            break;
-
-        case BIG_LIGHT:
-            gadget.points = 40;
-            break;
-    }
-
+    // Points based on type
+    gadget.points = GetGadgetPoints(gadget.type);
 
     gadget.active = true;
 
@@ -114,22 +91,15 @@ void DrawGadget(Gadget gadget)
 {
     if(gadget.active)
     {
-        DrawCircle(
-            gadget.position.x,
-            gadget.position.y,
-            20,
-            RED
-        );
+       DrawCircle(
+    gadget.position.x,
+    gadget.position.y,
+    20,
+    GetGadgetColor(gadget.type)
+);
     }
 }
 void ResetGadget(Gadget *gadget)
 {
-    gadget->type = GetRandomValue(0,4);
-
-    gadget->position.x = GetRandomValue(50,750);
-    gadget->position.y = -50;
-
-    gadget->speed = GetRandomValue(5,10);
-
-    gadget->active = true;
+    *gadget = CreateGadget();
 }
