0bc929cc7f66355533c51c1bf53167c04b0c8428
Zaafira Raisa <zfraisa2020@gmail.com>
2026-07-14 20:56:46 +0600

    Add CheckCoinCollision for coin pickup detection

--- src/DoraRush/coin.c
+++ src/DoraRush/coin.c
@@ -28,3 +28,29 @@ void DrawCoin(Coin *coin)
         DrawCircle(coin->x, coin->y, coin->radius, YELLOW);
     }
 }
+
+int CheckCoinCollision(Player *player, Coin *coin)
+{
+    if (coin->collected)
+    {
+        return 0;
+    }
+
+    Rectangle playerRect =
+    {
+        player->x,
+        player->y,
+        player->width,
+        player->height
+    };
+
+    Vector2 coinCenter = { coin->x, coin->y };
+
+    if (CheckCollisionCircleRec(coinCenter, coin->radius, playerRect))
+    {
+        coin->collected = 1;
+        return 1;
+    }
+
+    return 0;
+}
--- src/DoraRush/coin.h
+++ src/DoraRush/coin.h
@@ -10,11 +10,12 @@ typedef struct
     float y;
     float radius;
     int collected;
-    
+
 } Coin;
 
 void InitCoin(Coin *coin, Pipe *pipe);
 void UpdateCoin(Coin *coin, Pipe *pipe);
 void DrawCoin(Coin *coin);
+int CheckCoinCollision(Player *player, Coin *coin);
 
 #endif
