1dcfe9a2d72aee4111dbcffa4257e737e9b59fac
OnannaIslamFiha <fihaonannaislam01@gmail.com>
2026-07-14 17:53:03 +0600

    assign gadget values

--- src/GadgetInvaders/gadget_invaders.h
+++ src/GadgetInvaders/gadget_invaders.h
@@ -1,8 +1,17 @@
 #ifndef GADGET_INVADERS_H
 #define GADGET_INVADERS_H
+#include "raylib.h"
 
+//gadget type
+typedef enum
+{
+    ANYWHERE_DOOR,
+    BAMBOO_COPTER,
+    TIME_MACHINE,
+    SMALL_LIGHT,
+    BIG_LIGHT
 
-void RunGadgetInvaders();
-
+} GadgetType;
+ 
 
 #endif
