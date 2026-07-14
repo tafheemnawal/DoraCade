b87808bcf211838372b4399b41028900e6d95ce6
Zaafira Raisa <zfraisa2020@gmail.com>
2026-07-14 20:52:53 +0600

    Add Coin struct with init, update, draw

---
+++ src/DoraRush/coin.h
@@ -0,0 +1,20 @@
+#ifndef COIN_H
+#define COIN_H
+
+#include "player.h"
+#include "pipe.h"
+
+typedef struct
+{
+    float x;
+    float y;
+    float radius;
+    int collected;
+    
+} Coin;
+
+void InitCoin(Coin *coin, Pipe *pipe);
+void UpdateCoin(Coin *coin, Pipe *pipe);
+void DrawCoin(Coin *coin);
+
+#endif
