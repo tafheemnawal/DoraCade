6c53c1d6720417e1950179026afb21a7b99eadc5
Zaafira Raisa <zfraisa2020@gmail.com>
2026-08-25 20:04:59 +0600

    Add game over state on ghost collision with restart

--- src/DoraMan/test_main.c
+++ src/DoraMan/test_main.c
@@ -20,16 +20,35 @@ int main(void)
     InitGhost(&ghost, MAZE_ROWS - 2, MAZE_COLS - 2);
 
     int score = 0;
+    int gameOver = 0;
 
     while (!WindowShouldClose())
     {
         float dt = GetFrameTime();
-        UpdatePacPlayer(&pac, dt);
-        UpdateGhost(&ghost, dt);
 
-        if (CollectPellet(pac.row, pac.col))
+        if (!gameOver)
         {
-            score += 10;
+            UpdatePacPlayer(&pac, dt);
+            UpdateGhost(&ghost, dt);
+
+            if (CollectPellet(pac.row, pac.col))
+            {
+                score += 10;
+            }
+
+            if (CheckGhostCollision(&ghost, pac.x, pac.y))
+            {
+                gameOver = 1;
+            }
+        }
+
+        if (gameOver && IsKeyPressed(KEY_R))
+        {
+            InitPacPlayer(&pac);
+            InitPellets(pac.row, pac.col);
+            InitGhost(&ghost, MAZE_ROWS - 2, MAZE_COLS - 2);
+            score = 0;
+            gameOver = 0;
         }
 
         BeginDrawing();
@@ -41,6 +60,12 @@ int main(void)
         DrawGhost(&ghost);
 
         DrawText(TextFormat("Score: %d", score), 10, 10, 20, WHITE);
+
+        if (gameOver)
+        {
+            DrawText("GAME OVER", screenWidth / 2 - 120, screenHeight / 2 - 20, 40, RED);
+            DrawText("Press R to Restart", screenWidth / 2 - 100, screenHeight / 2 + 30, 20, WHITE);
+        }
 
         EndDrawing();
     }
