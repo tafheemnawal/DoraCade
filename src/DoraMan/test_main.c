b9d77a23243686610c2a81328c18f2e3ff634277
Zaafira Raisa <zfraisa2020@gmail.com>
2026-08-25 14:19:29 +0600

    Collect pellets and track score in game loop

--- src/DoraMan/test_main.c
+++ src/DoraMan/test_main.c
@@ -1,6 +1,7 @@
 #include <raylib.h>
 #include "maze.h"
 #include "pacplayer.h"
+#include "pellet.h"
 
 int main(void)
 {
@@ -12,17 +13,28 @@ int main(void)
 
     PacPlayer pac;
     InitPacPlayer(&pac);
+    InitPellets(pac.row, pac.col);
+
+    int score = 0;
 
     while (!WindowShouldClose())
     {
         float dt = GetFrameTime();
         UpdatePacPlayer(&pac, dt);
 
+        if (CollectPellet(pac.row, pac.col))
+        {
+            score += 10;
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
 
         DrawMaze();
+        DrawPellets();
         DrawPacPlayer(&pac);
+
+        DrawText(TextFormat("Score: %d", score), 10, 10, 20, WHITE);
 
         EndDrawing();
     }
